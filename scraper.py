6a9e1dd71ac18b738baf28f336af3011e2ea1a89
Nathaniel Parah <nathanielparah579@gmail.com>
2026-09-23 08:36:08 +0100

    add constructors, and attributes

--- scraper.py
+++ scraper.py
@@ -2,6 +2,11 @@ import request:
 from bs4 import BeautifulSoup
 
 class WebScraper:
-    def __init__
+    def __init__(self, target_url):
+        self.url = target_url
+        self.soup = None
+
+        
+
 
 
