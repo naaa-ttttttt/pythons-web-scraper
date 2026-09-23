a105670a2b0c100d848107e9b70cd21e5f31de63
Nathaniel Parah <nathanielparah579@gmail.com>
2026-09-23 08:59:43 +0100

    feat: write the request method that fetches contents

--- scraper.py
+++ scraper.py
@@ -1,12 +1,26 @@
-import request:
+import requests
 from bs4 import BeautifulSoup
 
 class WebScraper:
     def __init__(self, target_url):
         self.url = target_url
         self.soup = None
+    
+    def fetch_site(self):
+        try: 
+            print(f"Connecting to {self.url}...")
 
+            response = request.get(self.url, timeout= 20)
+
+            response.raise_for_status()
+
+            self.soup = BeautifulSoup(response.text, "html.parser")
+
+            print("Successfully connected and parsed the HTML")
         
+        except:
+            print(f"failed to fetch the page: {e}")
+
 
 
 
